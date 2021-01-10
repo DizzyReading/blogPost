747ca54fa4d2cfc266604bb06eb34e154c716966
DizzyReading <daintybeat@gmail.com>
2021-01-10 15:38:56 +0530

    step 1

--- app.js
+++ app.js
@@ -10,13 +10,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-
+app.get("/", function(req, res) {
+  res.render("home");
+});
 
 
 
